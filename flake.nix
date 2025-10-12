f83334dc7e00da7b356dbece054baa537b730bcf
HZ-9000 <HZ-9000@proton.me>
2025-10-11 18:13:09 -0700

    feat: rework for laptop

--- flake.nix
+++ flake.nix
@@ -1,71 +1,18 @@
 {
-  description = "NixOS systems and tools by HZ-9000";
-
   inputs = {
-    # Pin our primary nixpkgs repository. This is the main nixpkgs repository
-    # we'll use for our configurations. Be very careful changing this because
-    # it'll impact your entire system.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-
-    # We use the unstable nixpkgs repo for some packages.
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-
-    # Build a custom WSL installer
-    nixos-wsl.url = "github:nix-community/NixOS-WSL";
-    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
-
-    # snapd
-    nix-snapd.url = "github:nix-community/nix-snapd";
-    nix-snapd.inputs.nixpkgs.follows = "nixpkgs";
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # I think technically you're not supposed to override the nixpkgs
-    # used by neovim but recently I had failures if I didn't pin to my
-    # own. We can always try to remove that anytime.
-    neovim-nightly-overlay = {
-      url = "github:nix-community/neovim-nightly-overlay";
-    };
-
-    # Non-flakes
-    nvim-lspconfig.url = "github:neovim/nvim-lspconfig";
-    nvim-lspconfig.flake = false;
-    nvim-plenary.url = "github:nvim-lua/plenary.nvim";
-    nvim-plenary.flake = false;
-    nvim-telescope.url = "github:nvim-telescope/telescope.nvim/0.1.8";
-    nvim-telescope.flake = false;
-    nvim-treesitter.url = "github:nvim-treesitter/nvim-treesitter/v0.9.2";
-    nvim-treesitter.flake = false;
-    nvim-web-devicons.url = "github:nvim-tree/nvim-web-devicons";
-    nvim-web-devicons.flake = false;
+    # Use the Nixpkgs version that matches your system.stateVersion
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, home-manager, darwin, ... }@inputs: let
-    # Overlays is the list of overlays we want to apply from flake inputs.
-    overlays = [
-    ];
-
-    mkSystem = import ./lib/mksystem.nix {
-      inherit overlays nixpkgs inputs;
+  outputs = inputs@{ self, nixpkgs, ... }: {
+    # Replace 'nixos' with your actual system's hostname
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux"; # Or "aarch64-linux" for ARM
+      modules = [
+        # Import your existing configuration.nix
+	./configuration.nix
+        ./hardware-configuration.nix
+      ];
     };
-  in {
-    nixosConfigurations.vm-aarch64-prl = mkSystem "vm-aarch64-prl" {
-      system = "aarch64-linux";
-      user   = "hz-9000";
-    };
-
-    # nixosConfigurations.wsl = mkSystem "wsl" {
-    #   system = "x86_64-linux";
-    #   user   = "hz-9000";
-    #   wsl    = true;
-    # };
   };
 }
