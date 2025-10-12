b8e293f0810adff3237fec14ce2e51e1edeb5eba
HZ-9000 <HZ-9000@proton.me>
2025-10-12 15:19:16 -0700

    fix: add missing components from configuration.nix

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -3,8 +3,12 @@
   imports = [
     ./bootloader.nix
     ./system.nix
+    ./pipewire.nix
     ./user.nix
+    ./network.nix
     ./hardware.nix
+    ./services.nix
+    ./xserver.nix
     ./wayland.nix
   ];
 }
