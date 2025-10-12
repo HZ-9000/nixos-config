f394c2e663f237bfead2fb8eee8527b0e59f468c
HZ-9000 <HZ-9000@proton.me>
2025-10-11 23:01:34 -0700

    fix: move hardware config

--- flake.nix
+++ flake.nix
@@ -4,15 +4,23 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }: {
+  outputs = 
+    inputs@{ 
+      self, 
+      nixpkgs, 
+      ... 
+  }: 
+  {
     # Replace 'nixos' with your actual system's hostname
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux"; # Or "aarch64-linux" for ARM
-      modules = [
-        # Import your existing configuration.nix
-	./configuration.nix
-        ./hardware-configuration.nix
-      ];
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux"; # Or "aarch64-linux" for ARM
+        modules = [
+          # Import your existing configuration.nix
+	  ./configuration.nix
+          ./hosts/stormlight/hardware-configuration.nix
+        ];
+      };
     };
   };
 }
