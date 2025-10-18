7341c87d141a228fecee6f0ff462da3dceeecf05
HZ-9000 <HZ-9000@proton.me>
2025-10-18 00:24:40 -0700

    feat: add new wallpaper

--- modules/home/hyprland/hyprlock.nix
+++ modules/home/hyprland/hyprlock.nix
@@ -10,17 +10,17 @@
     package = inputs.hyprlock.packages.${pkgs.system}.hyprlock;
 
     settings = {
-      # background = [
-      #   {
-      #     monitor = "";
-      #     path = "${../../../wallpapers/forest_road.jpg}";
-      #     blur_passes = 2;
-      #     contrast = 0.8916;
-      #     brightness = 0.8172;
-      #     vibrancy = 0.1696;
-      #     vibrancy_darkness = 0.0;
-      #   }
-      # ];
+      background = [
+        {
+          monitor = "";
+          path = "${../../../wallpapers/framework_dunes.jpeg}";
+          blur_passes = 2;
+          contrast = 0.8916;
+          brightness = 0.8172;
+          vibrancy = 0.1696;
+          vibrancy_darkness = 0.0;
+        }
+      ];
     };
   };
 }
