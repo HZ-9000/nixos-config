8849fc7265eb07b8ac1912497b92ee4ce540234d
HZ-9000 <HZ-9000@proton.me>
2026-08-27 13:02:49 -0700

    feat: add SSH configuration and security settings for gnome-keyring integration

---
+++ home/base/tui/ssh.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  ...
+}:
+{
+  programs.ssh = {
+    enable = true;
+
+    enableDefaultConfig = false;
+    settings."*" = {
+      ForwardAgent = false;
+      AddKeysToAgent = "yes";
+      Compression = true;
+      ServerAliveInterval = 0;
+      ServerAliveCountMax = 3;
+      HashKnownHosts = false;
+      UserKnownHostsFile = "~/.ssh/known_hosts";
+      ControlMaster = "no";
+      ControlPath = "~/.ssh/master-%r@%n:%p";
+      ControlPersist = "no";
+    };
+
+    settings = {
+      "github.com" = {
+        HostName = "ssh.github.com";
+        Port = 443;
+        User = "git";
+        IdentitiesOnly = true;
+      };
+    };
+  };
+}
