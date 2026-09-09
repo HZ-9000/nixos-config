79bc1ca47e97c25cf8984c4028a0b9a4e274c9e2
HZ-9000 <HZ-9000@proton.me>
2026-09-08 21:03:08 -0700

    fix: update stormlight configuration to set SSH identity file for GitHub

--- home/hosts/linux/stormlight.nix
+++ home/hosts/linux/stormlight.nix
@@ -1,8 +1,13 @@
 { ... }:
+let 
+  hostName = "stormlight";
+in 
 {
   disabledModules = [
     ../../linux/base/sops.nix
   ];
+
+  programs.ssh.settings."github.com".IdentityFile = "${config.home.homeDirectory}/.ssh/${hostName}";
 
   imports = [ ../../linux/default.nix ];
 }
