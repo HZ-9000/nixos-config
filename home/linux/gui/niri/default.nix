5c859151499d33a7387005df5722861ef0c31202
hz-9000 <hz-9000@proton.me>
2026-09-12 11:01:10 -0700

    fix: cleanup niri

--- home/linux/gui/niri/default.nix
+++ home/linux/gui/niri/default.nix
@@ -1,8 +1,49 @@
-{ ... }:
+{
+  pkgs,
+  config,
+  ...
+}:
 {
   imports = [
-    ./niri.nix
     ./config.nix
     ./variables.nix
   ];
+
+  home.packages = with pkgs; [
+    xwayland-satellite
+
+    slurp
+    grim
+    satty
+  ];
+
+  systemd.user.services.niri-flake-polkit = {
+    Unit = {
+      Description = "PolicyKit Authentication Agent provided by niri-flake";
+      After = [
+        "graphical-session.target"
+      ];
+      Wants = [ "graphical-session-pre.target" ];
+    };
+    Install.WantedBy = [ "niri.service" ];
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
+
+  # NOTE: this executable is used by greetd to start a wayland session when system boot up
+  # with such a vendor-no-locking script, we can switch to another wayland compositor without modifying greetd's config in NixOS module
+  home.file.".wayland-session" = {
+    source = pkgs.writeScript "init-session" ''
+      # trying to stop a previous niri session
+      systemctl --user is-active niri.service && systemctl --user stop niri.service
+      # and then we start a new one
+      /run/current-system/sw/bin/niri-session
+    '';
+    executable = true;
+  };
 }
