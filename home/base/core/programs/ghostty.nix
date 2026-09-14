478498416aca0532fcf964733a934f8a981237e1
hz-9000 <hz-9000@proton.me>
2026-09-14 10:51:38 -0700

    fix: ghostty opacity

--- home/base/core/programs/ghostty.nix
+++ home/base/core/programs/ghostty.nix
@@ -7,7 +7,7 @@
         "JetBrainsMono Nerd Font"
       ];
       theme = "Rosé Pine Moon";
-      background-opacity = 0.82;
+      background-opacity = 0.55;
       background-opacity-cells = true;
       window-decoration = false;
       gtk-titlebar = false;
