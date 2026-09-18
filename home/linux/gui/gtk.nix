e5dca07a1f74ca1c04cab2c6be61213c6a0a320e
hz-9000 <hz-9000@proton.me>
2026-09-17 22:06:00 -0700

    wip: keep working on thunar

--- home/linux/gui/gtk.nix
+++ home/linux/gui/gtk.nix
@@ -21,11 +21,61 @@
 
   xdg.configFile."gtk-3.0/gtk.css".force = true;
   xdg.configFile."gtk-3.0/thunar.css".text = ''
-    window.thunar,
+    window.thunar {
+      background-color: alpha(@window_bg_color, 0.68);
+      color: @window_fg_color;
+    }
+
     window.thunar .view,
     window.thunar treeview,
-    window.thunar iconview {
-      background-color: alpha(@window_bg_color, 0.55);
+    window.thunar iconview,
+    window.thunar viewport,
+    window.thunar .sidebar {
+      background-color: transparent;
+      color: @view_fg_color;
+    }
+
+    window.thunar toolbar,
+    window.thunar .toolbar {
+      background-color: transparent;
+      color: @headerbar_fg_color;
+      border-color: transparent;
+      box-shadow: none;
+    }
+
+    window.thunar headerbar {
+      min-height: 0;
+      padding: 0;
+      margin: 0;
+      border: 0;
+      background: transparent;
+      box-shadow: none;
+    }
+
+    window.thunar entry,
+    window.thunar entry.search {
+      background-color: alpha(@window_bg_color, 0.38);
+      color: @window_fg_color;
+      border-color: alpha(@accent_color, 0.35);
+      box-shadow: none;
+    }
+
+    window.thunar entry:focus,
+    window.thunar entry.search:focus {
+      background-color: alpha(@window_bg_color, 0.48);
+      border-color: alpha(@accent_color, 0.7);
+    }
+
+    window.thunar .view:selected,
+    window.thunar treeview:selected,
+    window.thunar iconview:selected {
+      background-color: alpha(@accent_bg_color, 0.78);
+      color: @accent_fg_color;
+    }
+
+    window.thunar image {
+      -gtk-icon-effect: none;
+      opacity: 1;
     }
   '';
 
