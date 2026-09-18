6ef65f389eeb360353993ef079a3198a2c55142b
hz-9000 <hz-9000@proton.me>
2026-09-17 21:51:51 -0700

    feat: make thunar transparent

--- home/linux/gui/gtk.nix
+++ home/linux/gui/gtk.nix
@@ -4,6 +4,10 @@
 
   gtk = {
     enable = true;
+    gtk3.extraCss = ''
+      @import url("noctalia.css");
+      @import url("thunar.css");
+    '';
     font = {
       name = "Jetbrains Mono";
       size = 12;
@@ -14,6 +18,16 @@
       size = 24;
     };
   };
+
+  xdg.configFile."gtk-3.0/gtk.css".force = true;
+  xdg.configFile."gtk-3.0/thunar.css".text = ''
+    window.thunar,
+    window.thunar .view,
+    window.thunar treeview,
+    window.thunar iconview {
+      background-color: alpha(@window_bg_color, 0.55);
+    }
+  '';
 
   home.pointerCursor = {
     enable = true;
